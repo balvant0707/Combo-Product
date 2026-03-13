b00f9ac337edfa8b2f5b64c0393830d14e785027
Balvant <balvant@m2webdesigning.com>
2026-03-13 12:53:20 +0530

    13-03-2026

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -20,7 +20,7 @@ export default function App() {
   return (
     <AppProvider embedded apiKey={apiKey}>
       <s-app-nav>
-        <s-link href="/app">Dashboard</s-link>
+        {/* <s-link href="/app">Dashboard</s-link> */}
         <s-link href="/app/boxes">Manage Boxes</s-link>
         <s-link href="/app/analytics">Analytics</s-link>
         <s-link href="/app/settings">Settings</s-link>
